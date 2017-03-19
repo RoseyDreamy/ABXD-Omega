bb8d9bfaa22d9c9623703bc14f78f7b8c01e788a
Jamie Dignam <cosmiceternity05@gmail.com>
2017-03-19 13:45:35 +0000

    Remove rboard copyright and rev date, add buttons
    I decided to add the buttons back to the footer that were, for no reason, removed.
    Removed the Rosey's Board-specific text, too.

--- footer.php
+++ footer.php
@@ -5,12 +5,18 @@ else echo '<a href="#" onclick="enableMobileLayout(-1); return false;" rel="nofo
 <br>
 <br>
 <?php $bucket = "footer"; include("./lib/pluginloader.php");?>
-Powered by AcmlmBoard XD - version 3.0+&#937; (2017-3-13)<br />
+Powered by AcmlmBoard XD - version 3.0+&#937;<br />
 By Dirbaio, xfix, Kawa, StapleButter, Nina, et al<br />
-Rosey's Board &copy; RoseyDreamy<br />
 AcmlmBoard &copy; Jean-Fran&ccedil;ois Lapointe<br />
 <?php print __("<!-- English translation by The ABXD Team -->")?>
-
+<a href="http://validator.w3.org/check?uri=referer">
+	<img src="img/xhtml10.png" alt="Valid XHTML 1.0 Transitional" />
+</a>
+<a href="http://jigsaw.w3.org/css-validator/">
+	<img src="img/css.png" alt="Valid CSS!" />
+</a>
+<a href="https://github.com/timekiller89/ABXD-Omega">
+	<img src="img/getabxd.png" alt="Get a copy for yourself" />
+</a>
 <?php print (isset($footerButtons) ? $footerButtons : "")?>
 <?php print (isset($footerExtensionsB) ? $footerExtensionsB : "")?>
-
