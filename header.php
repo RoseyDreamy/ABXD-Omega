c41d86077157cb2eb7ed114dd6b11e58a57c85f3
Jamie Dignam <cosmiceternity05@gmail.com>
2017-04-09 14:11:55 +0100

    iOS web app capability
    Allows the board to be saved to the home screen, and work almost like a native app. It's used on rboard and works fairly well.

--- header.php
+++ header.php
@@ -1,16 +1,39 @@
 	<meta http-equiv="Content-Type" content="text/html; CHARSET=utf-8" />
 	<meta name="description" content="<?php print Settings::get('metaDescription'); ?>" />
 	<meta name="keywords" content="<?php print Settings::get('metaTags'); ?>" />
+	<meta name="apple-mobile-web-app-capable" content="yes">
 	<link rel="shortcut icon" type="image/x-icon" href="<?php print $layout_favicon;?>" />
 	<link rel="stylesheet" type="text/css" href="<?php print resourceLink("css/common.css");?>" />
 	<link rel="stylesheet" type="text/css" href="<?php print resourceLink("js/spectrum.css");?>" />
 	<link rel="stylesheet" href="<?php print resourceLink("css/font-awesome.min.css");?>">
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
 	
 	<script type="text/javascript" src="<?php print resourceLink("js/jquery.js");?>"></script>
 	<script type="text/javascript" src="<?php print resourceLink("js/tricks.js");?>"></script>
 	<script type="text/javascript" src="<?php print resourceLink("js/jquery.tablednd_0_5.js");?>"></script>
 	<script type="text/javascript" src="<?php print resourceLink("js/jquery.scrollTo-1.4.2-min.js");?>"></script>
 	<script type="text/javascript" src="<?php print resourceLink("js/spectrum.js");?>"></script>
+<script>
+    (function(document,navigator,standalone) {
+        // prevents links from apps from oppening in mobile safari
+        // this javascript must be the first script in your <head>
+        if ((standalone in navigator) && navigator[standalone]) {
+            var curnode, location=document.location, stop=/^(a|html)$/i;
+            document.addEventListener('click', function(e) {
+                curnode=e.target;
+                while (!(stop).test(curnode.nodeName)) {
+                    curnode=curnode.parentNode;
+                }
+                // Conditions to do this only on links to your own app
+                // if you want all links, use if('href' in curnode) instead.
+                if('href' in curnode && ( curnode.href.indexOf('http') || ~curnode.href.indexOf(location.host) ) ) {
+                    e.preventDefault();
+                    location.href = curnode.href;
+                }
+            },false);
+        }
+    })(document,window.navigator,'standalone');
+</script>
 	<script type="text/javascript">
 		boardroot = <?php print json_encode($boardroot); ?>;
 	</script>
